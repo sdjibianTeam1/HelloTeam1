341cace17a95705d913327d139989806fafd0f4f
sdjibian <sdjibian@student.tgm.ac.at>
2018-02-13 11:41:42 +0100

    Erster Commit.

---
+++ src/Djibian/HelloTeam.java
@@ -0,0 +1,11 @@
+package Djibian;
+
+public class HelloTeam {
+	public static void hallovonSako() {
+		System.out.print("Schönes Sommersemester wünscht Sako Djibian!!!");
+	}
+	public static void main(String[] args) {
+		hallovonSako();
+	}
+
+}
