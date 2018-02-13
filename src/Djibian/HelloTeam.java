cc3e5f9b0e5ddaac9c38cbf25c4b6b7134ff21a1
shediyehloo <shediyehloo@student.tgm.ac.at>
2018-02-13 13:16:09 +0100

    Sams Commit

--- src/Djibian/HelloTeam.java
+++ src/Djibian/HelloTeam.java
@@ -2,7 +2,7 @@ package Djibian;
 
 public class HelloTeam {
 	public static void hallovonSako() {
-		System.out.print("Schönes Sommersemester wünscht Sako Djibian!!!");
+		System.out.print("Schönes Sommersemester wünscht Sam!!!");
 	}
 	public static void main(String[] args) {
 		hallovonSako();
