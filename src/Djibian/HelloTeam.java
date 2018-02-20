94f33e6049c05438e751af8b32cf11711da54bc3
dcerny01 <35727585+dcerny01@users.noreply.github.com>
2018-02-20 14:01:50 +0100

    Update HelloTeam.java

--- src/Djibian/HelloTeam.java
+++ src/Djibian/HelloTeam.java
@@ -8,10 +8,14 @@ public class HelloTeam {
 	public static void hallovonTobi() {
 		System.out.print("Schönes Sommersemester wünscht Tobi!!!");
 	}
+	public static void hallovonDome() {
+		System.out.print("Schönes Sommersemester wünscht Dome der Boss der Bosse!!!");
+	}
 	
 	public static void main(String[] args) {
 		hallovonSako();
 		hallovonTobi();
+		hallovonDome();
 	}
 
 }
